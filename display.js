bf8b456b8863dcedd690b04eb620456b5d9a9f39
YosefTA <50155689+YosefTA@users.noreply.github.com>
2020-05-31 15:24:16 -0400

    display
    put all display js in one file

--- display.js
+++ display.js
@@ -1,6 +1,19 @@
-var width = 300, height = 600;
+//initialize 
+var columns = 10, rows = 20, width = 300, height = 600;
 var cellWidth =  width / columns, cellheight = height / rows;
 var hideBoard = false;
+
+var board =[];
+
+//turn "board" into a 20 * 10 array
+function writeBoard() {
+	for ( i = 0; i < rows; i++ ){
+		board[i] = [];
+			for ( j = 0; j < columns; j++ ) {				
+			board[i][j] = 0;
+		}
+	}
+}
 
 function drawBlock( x, y, ctx ) {
 	ctx.fillRect( cellWidth * x, cellheight * y, cellWidth -1 , cellheight -1 );
@@ -22,22 +35,5 @@ function render() {
 				drawBlock( x, y, ctx );
 			}
 		}
-	}
-}
-
-setInterval( render, 30 );
-
-function sideDisplay( id, piece ) {
-	var canvas = document.getElementById( id );
-	var ctx = canvas.getContext( '2d' );
-	
-	ctx.fillStyle = "white";
-	ctx.fillRect( 0, 0, cellWidth * 5, cellheight * 4 );
-
-	var copy = JSON.parse( JSON.stringify( shapes[ piece - 1 ] ) );
-	
-	ctx.fillStyle = colors[ piece - 1 ];
-	for ( var i = 0; i < copy.length; i++ ) {
-		drawBlock( copy[i].x - 3 , copy[i].y  + 1, ctx );
 	}
 }
